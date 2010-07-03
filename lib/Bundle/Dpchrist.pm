0738d4f05d6a9e4a4c89afab0cb6fa48fa0d3cc7
David Paul Christensen <dpchrist@holgerdanske.com>
2010-07-03 05:36:33 +0000

    Import of DPCHRIST/Bundle-Dpchrist-1.017 from CPAN.
    gitpan-cpan-distribution: Bundle-Dpchrist
    gitpan-cpan-version:      1.017
    gitpan-cpan-path:         DPCHRIST/Bundle-Dpchrist-1.017.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Dpchrist.pm
@@ -0,0 +1,70 @@
+#######################################################################
+# $Id: Dpchrist.pm,v 1.17 2010-07-03 05:01:56 dpchrist Exp $
+#######################################################################
+# package:
+#----------------------------------------------------------------------
+
+package Bundle::Dpchrist;
+
+our $VERSION = sprintf("%d.%03d", q$Revision: 1.17 $ =~ /(\d+)/g);
+
+1;
+
+__END__
+
+#######################################################################
+
+=head1 NAME
+
+Bundle::Dpchrist - David Christensen's Perl modules
+
+
+=head1 CONTENTS
+
+    Dpchrist::Module			1.018
+
+    Dpchrist::Is			1.017
+    Dpchrist::LangUtil			1.003
+    Dpchrist::Term			1.008
+    Dpchrist::Timestamp			1.011
+
+    Dpchrist::File::Append		1.026
+    Dpchrist::Tag			1.029
+
+    Dpchrist::Debug			1.038
+
+    Dpchrist::Cvs::NerfTags		1.004
+    Dpchrist::Directory			1.009
+    Dpchrist::File::Find::Duplicates	1.007
+    Dpchrist::File::LimitPath		1.027
+    Dpchrist::File::Newest		1.023
+    Dpchrist::ShellScript		1.005
+    Dpchrist::TriangularNumber		1.015
+
+
+=head1 AUTHOR
+
+David Paul Christensen  dpchrist@holgerdanske.com
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2010 by David Paul Christensen  dpchrist@holgerdanske.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307,
+USA.
+
+=cut
+
+#######################################################################
